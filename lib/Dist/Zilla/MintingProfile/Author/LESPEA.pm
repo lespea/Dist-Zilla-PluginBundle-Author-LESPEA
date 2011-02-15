7b96c709fa1d8281499c906c125ef786957cb59a
Adam Lesperance <lespea@gmail.com>
2011-02-15 14:24:13 -0600

    Switch minting profile to my package/style

--- lib/Dist/Zilla/MintingProfile/Author/LESPEA.pm
+++ lib/Dist/Zilla/MintingProfile/Author/LESPEA.pm
@@ -1,9 +1,11 @@
+package Dist::Zilla::MintingProfile::Author::LESPEA;
+
 use strict;
 use warnings;
 
-package Dist::Zilla::MintingProfile::Author::KENTNL;
+# VERSION
 
-# ABSTRACT: KENTNL's Minting Profile
+# ABSTRACT: LESPEA's Minting Profile
 
 use Moose;
 use namespace::autoclean;
@@ -11,10 +13,12 @@ with 'Dist::Zilla::Role::MintingProfile::ShareDir';
 
 =head1 SYNOPSIS
 
-    dzil new -P Author::KENTNL Some::Dist::Name
+    dzil new -P Author::LESPEA Some::Dist::Name
 
 =cut
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
+
+# Magic
 1;
