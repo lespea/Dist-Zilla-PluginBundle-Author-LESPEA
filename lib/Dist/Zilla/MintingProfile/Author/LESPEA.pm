40bf1db93aa1bcbd6d7371437e5a54c83c9042d3
Adam Lesperance <lespea@gmail.com>
2011-02-16 09:25:05 -0600

    Reorder file + fix Synopsis

--- lib/Dist/Zilla/MintingProfile/Author/LESPEA.pm
+++ lib/Dist/Zilla/MintingProfile/Author/LESPEA.pm
@@ -1,9 +1,9 @@
-package Dist::Zilla::MintingProfile::Author::LESPEA;
-
 use strict;
 use warnings;
 
-# VERSION
+use utf8;
+
+package Dist::Zilla::MintingProfile::Author::LESPEA;
 
 # ABSTRACT: LESPEA's Minting Profile
 
@@ -11,14 +11,24 @@ use Moose;
 use namespace::autoclean;
 with 'Dist::Zilla::Role::MintingProfile::ShareDir';
 
+
+=encoding utf8
+
 =head1 SYNOPSIS
 
+    #   On the command line run:
     dzil new -P Author::LESPEA Some::Dist::Name
 
+
+=head1 DESCRIPTION
+
+This installs a Dist::Zilla profile to start a new module with
+
 =cut
+
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
 
-# Magic
+# Happy ending
 1;
